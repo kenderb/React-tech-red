6f337f74d605cc744cc2cc30136168e9cc1d8795
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-26 08:35:34 -0500

    Burn type User

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,13 +5,12 @@ import { Redirect } from 'react-router-dom';
 import { authUser } from '../actions';
 
 const Login = ({ user, authUser }) => {
-  const [type, setType] = useState('User');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handelSubmit = e => {
     e.preventDefault();
-    authUser({ email, password, type });
+    authUser({ email, password, type: 'User' });
   };
 
   if (user.isLoggedIn) {
@@ -20,7 +19,6 @@ const Login = ({ user, authUser }) => {
   return (
     <main>
       <form onSubmit={e => handelSubmit(e)}>
-        <input onChange={e => setType(e.target.value)} type="text" placeholder="User name" value="User" />
         <input onChange={e => setEmail(e.target.value)} type="email" placeholder="email" />
         <input onChange={e => setPassword(e.target.value)} type="password" placeholder="password" />
         <button type="submit">Login</button>
