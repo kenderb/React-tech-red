78ac1211cda9a6aa230f65b35e2ce6935944c487
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-27 20:06:19 -0500

    Add a shadow effect for the NavHeader

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -5,12 +5,12 @@ import PropTypes from 'prop-types';
 import hamburger from '../assets/images/Hamburguesa.svg';
 import { toggleSideBar } from '../actions';
 
-const NavHeader = ({ toggleSideBar }) => {
+const NavHeader = ({ sideBar, toggleSideBar }) => {
   useEffect(() => {
     toggleSideBar();
   }, []);
   return (
-    <header className="header-container">
+    <header className={`header-container ${!sideBar ? 'hard-shadow' : ''}`}>
       <button type="button" onClick={() => toggleSideBar()} className="hamburguer-button">
         <img src={hamburger} alt={hamburger} />
       </button>
@@ -18,7 +18,11 @@ const NavHeader = ({ toggleSideBar }) => {
   );
 };
 
+const mapStateToProps = state => ({
+  sideBar: state.sideBar,
+});
 NavHeader.propTypes = {
+  sideBar: PropTypes.bool.isRequired,
   toggleSideBar: PropTypes.func.isRequired,
 };
-export default connect(null, { toggleSideBar })(NavHeader);
+export default connect(mapStateToProps, { toggleSideBar })(NavHeader);
